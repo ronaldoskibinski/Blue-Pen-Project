f688a7683914a67b6084e1ba702e19f8676577a4
Fernando Stadler <fernandostadler2@gmail.com>
2023-02-25 16:20:46 -0300

    Criado a função referente ao for

--- appBluepen.js
+++ appBluepen.js
@@ -1,2 +1,5 @@
-import MeuAmigo from "./libs.js";
-MeuAmigo("Olá amigos!");
+import * as canetaAzul from "./libs.js";
+
+let data = canetaAzul.CriarArray('8', '7', '2', '5', '5', '9', '9');
+
+const loop = canetaAzul.MeEntregaEla(data, "igual", "8");
